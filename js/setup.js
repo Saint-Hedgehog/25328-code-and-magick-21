138a4276be28a3379ab995ca3c928d582b15bcf4
Sainthedgehog1 <svyatoiej@gmail.com>
2020-09-25 21:23:27 +0300

    module3-task1

--- js/setup.js
+++ js/setup.js
@@ -50,12 +50,12 @@ const randomNumber = (min, max) => Math.floor(Math.random() * (max - min)) + min
 
 // функция отрисовки персонажей
 const getWizards = () => {
-  let nameIndex = randomNumber(0, WIZARD_NAMES.length - 1);
-  let surnameIndex = randomNumber(0, WIZARD_SURNAMES.length - 1);
-  let coatColorIndex = randomNumber(0, COAT_COLORS.length - 1);
-  let eyesColorIndex = randomNumber(0, EYES_COLORS.length - 1);
+  const nameIndex = randomNumber(0, WIZARD_NAMES.length - 1);
+  const surnameIndex = randomNumber(0, WIZARD_SURNAMES.length - 1);
+  const coatColorIndex = randomNumber(0, COAT_COLORS.length - 1);
+  const eyesColorIndex = randomNumber(0, EYES_COLORS.length - 1);
   return {
-    name: WIZARD_NAMES[nameIndex] + ` ` + WIZARD_SURNAMES[surnameIndex],
+    name: `${WIZARD_NAMES[nameIndex]} ${WIZARD_SURNAMES[surnameIndex]}`,
     coatColor: COAT_COLORS[coatColorIndex],
     eyesColor: EYES_COLORS[eyesColorIndex]
   };
@@ -66,20 +66,21 @@ const similarWizardTemplate = document.querySelector(`#similar-wizard-template`)
 
 // функция создает копию шаблона и вставляет в него случайное имя с фамилией, цвет мантии и цвет глаз
 const renderWizard = (wizard) => {
-  let wizardElement = similarWizardTemplate.cloneNode(true);
-
-  wizardElement.querySelector(`.setup-similar-label`).textContent = wizard.name;
-  wizardElement.querySelector(`.wizard-coat`).style.fill = wizard.coatColor;
-  wizardElement.querySelector(`.wizard-eyes`).style.fill = wizard.eyesColor;
+  const wizardElement = similarWizardTemplate.cloneNode(true);
+  const {name, coatColor, eyesColor} = wizard;
+  wizardElement.querySelector(`.setup-similar-label`).textContent = name;
+  wizardElement.querySelector(`.wizard-coat`).style.fill = coatColor;
+  wizardElement.querySelector(`.wizard-eyes`).style.fill = eyesColor;
 
   return wizardElement;
 };
 
 // вставляет в пустой массив wizards элементы из функции getWizards()
 const generateWizards = (wizardCount) => {
-  let wizards = [];
+  const wizards = [];
+
   for (let i = 0; i < wizardCount; i++) {
-    wizards[i] = getWizards();
+    wizards.push(getWizards());
   }
   return wizards;
 };
@@ -91,13 +92,13 @@ const wizards = generateWizards(WIZARD_NUMBER);
 const fragment = document.createDocumentFragment();
 
 // Отрисовывает шаблон в документ
-const getWizardTemplate = (arr) => {
-  for (let i = 0; i < arr.length; i++) {
-    fragment.appendChild(renderWizard(wizards[i]));
-  }
+const getWizardTemplate = () => {
+  wizards.forEach((wizard) => {
+    fragment.appendChild(renderWizard(wizard));
+  });
 };
 
-getWizardTemplate(wizards);
+getWizardTemplate();
 
 // находит элемент, в который мы будем вставлять похожих магов
 userDialog.querySelector(`.setup-similar-list`).appendChild(fragment);
