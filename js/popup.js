e357eb9542647773d15b7a7322d7d5f4e2e74ff7
Sainthedgehog1 <svyatoiej@gmail.com>
2020-10-06 17:52:24 +0300

    module5-task1

--- js/popup.js
+++ js/popup.js
@@ -4,11 +4,13 @@
 
   const {setupUserName, userDialog, wizardCoat, wizardEyes, wizardFireball, setupOpen, setupClose, dialogHandle} = window.elements;
   const {getWizardCoatColor, getWizardEyesColor, getFireballColor} = window.colorize;
+  const {isEscEvent, isEnterEvent} = window.util;
+  const {moveOn} = window.move;
 
   // Модальное окно
   const onPopupEscPress = (evt) => {
     if (!setupUserName.matches(`:focus`)) {
-      window.util.isEscEvent(evt, closePopup);
+      isEscEvent(evt, closePopup);
     }
   };
 
@@ -35,7 +37,7 @@
   });
 
   setupOpen.addEventListener(`keydown`, (evt) => {
-    window.util.isEnterEvent(evt, openPopup);
+    isEnterEvent(evt, openPopup);
   });
 
   setupClose.addEventListener(`click`, () => {
@@ -43,57 +45,12 @@
   });
 
   setupClose.addEventListener(`keydown`, (evt) => {
-    window.util.isEnterEvent(evt, closePopup);
+    isEnterEvent(evt, closePopup);
   });
 
   // Перетаскивание модального окна
   dialogHandle.addEventListener(`mousedown`, (evt) => {
-    evt.preventDefault();
-
-    let startCoords = {
-      x: evt.clientX,
-      y: evt.clientY
-    };
-
-    let dragged = false;
-
-    const onMouseMove = (moveEvt) => {
-      moveEvt.preventDefault();
-
-      dragged = true;
-
-      const shift = {
-        x: startCoords.x - moveEvt.clientX,
-        y: startCoords.y - moveEvt.clientY
-      };
-
-      startCoords = {
-        x: moveEvt.clientX,
-        y: moveEvt.clientY
-      };
-
-      userDialog.style.top = `${userDialog.offsetTop - shift.y}px`;
-      userDialog.style.left = `${userDialog.offsetLeft - shift.x}px`;
-
-    };
-
-    const onMouseUp = (upEvt) => {
-      upEvt.preventDefault();
-
-      document.removeEventListener(`mousemove`, onMouseMove);
-      document.removeEventListener(`mouseup`, onMouseUp);
-
-      if (dragged) {
-        const onClickPreventDefault = (clickEvt) => {
-          clickEvt.preventDefault();
-          dialogHandle.removeEventListener(`click`, onClickPreventDefault);
-        };
-        dialogHandle.addEventListener(`click`, onClickPreventDefault);
-      }
-    };
-
-    document.addEventListener(`mousemove`, onMouseMove);
-    document.addEventListener(`mouseup`, onMouseUp);
+    moveOn(evt);
   });
 
 })();
